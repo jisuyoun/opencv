1686b9e64b1e18c1c1670ed5e8611779897d4b00
jisuyoun <jisuyoun319@gmail.com>
2025-05-01 19:06:42 +0900

    initial setting

---
+++ src/config.py
@@ -0,0 +1,8 @@
+# kafka 브로커 주소
+KAFKA_BOOTSTRAP_SERVERS = 'localhost:9092'
+
+# 불량 알림 메시지를 보낼 Kafka 토픽
+DEFECT_ALERTS_TOPIC = 'product_defect_alerts'
+
+# Consumer 그룹 ID
+CONSUMER_GROUP_ID = 'quality-check-group'
